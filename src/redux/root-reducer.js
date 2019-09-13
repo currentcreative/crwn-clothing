8d2ae30b66677d6d5268260b8eae1e0064096342
Nathan Snyder <nate@nathans-mini.home>
2019-09-12 22:11:41 -0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+//tells redux-persist you want to use local storage
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+//specifying redux-persist; Firebase is handling users, so we just want to whitelist 'cart'
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+// redux-persist pt. 3 - must set our combineReducers to 'rootReducer'
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
+
+//redux-persist pt. 4 - export both above settings with persistReducer()
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// from redux-persist so we can use local storage in browser
+export const persistor = persistStore(store);
+
+export default { store, persistor };
